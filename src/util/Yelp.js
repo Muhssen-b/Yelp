24440e2b840f830fde3cc1b59ab07eab395ef258
Muhssen-b <muhssen.b@outlook.com>
2020-09-22 21:20:17 +1000

    First Commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,34 @@
+//const { default: SearchBar } = require("../components/SearchBar/SearchBar");
+
+const apiKey = 'Q_S05Fdvlu_hdbU6swBba6dUURUL1CgDn1s8jTbLbHKDrjWrbkgn_qreLWJbTcunLzdfxO8ABx4x9j3n_oY2WaWCMHkBclcE0b25jUgAeSoODHoI1mzH_3_87VxPX3Yx'
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+           headers: {
+            Authorization : `Bearer ${apiKey}`
+            }
+          })
+          .then((response) => {
+            return response.json();
+          })
+          .then(jsonResponse => {
+          if(jsonResponse.businesses) {
+              return jsonResponse.businesses.map(business => ({
+                 id: business.id,
+                 imageSrc: business.image_url,
+                 name: business.name,
+                 address: business.location.address1,
+                 city: business.location.city,
+                 state: business.location.state,
+                 zipCode: business.location.zip_code,
+                 category: business.categories[0].title,
+                 rating: business.rating,
+                 reviewCount: business.review_count
+            }));
+          }
+        })
+    }
+  }
+
+export default Yelp;
